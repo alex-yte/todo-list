a51e6509f67650c2614740fc89e1f320f8c48dd2
Denis Chirkov <den.alex.chirkov@gmail.com>
2023-07-22 11:22:44 +0200

    added an array for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "./App.css";
 import Todolist from "./Todolist";
 
 function App() {
+  let tasks1 = [
+    { id: 1, title: "CSS", isDone: true },
+    { id: 2, title: "HTML", isDone: true },
+    { id: 3, title: "React", isDone: false },
+  ];
+  let tasks2 = [
+    { id: 1, title: "Terminal", isDone: false },
+    { id: 2, title: "Resident", isDone: true },
+    { id: 3, title: "Jango", isDone: true },
+  ];
+
   return (
-    <div className="App">
-      <Todolist />
-      <Todolist />
-      <Todolist />
+    <div>
+      <div className="App">
+        <Todolist title="What to learn" tasks={tasks1} />
+        <Todolist title="Movies" tasks={tasks2} />
+      </div>
     </div>
   );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,23 +1,34 @@
-function Todolist() {
+type TaskType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+type PropsType = {
+  title: string;
+  tasks: Array<TaskType>;
+};
+
+function Todolist(props: PropsType) {
   return (
     <div>
-      <h3>What to learn</h3>
+      <h3>{props.title}</h3>
       <div>
         <input type="text" />
         <button>+</button>
       </div>
       <ul>
         <li>
-          <input type="checkbox" checked={true} />
-          <span>HTML</span>
+          <input type="checkbox" checked={props.tasks[0].isDone} />
+          <span>{props.tasks[0].title}</span>
         </li>
         <li>
-          <input type="checkbox" checked={true} />
-          <span>CSS</span>
+          <input type="checkbox" checked={props.tasks[1].isDone} />
+          <span>{props.tasks[1].title}</span>
         </li>
         <li>
-          <input type="checkbox" checked={false} />
-          <span>React</span>
+          <input type="checkbox" checked={props.tasks[2].isDone} />
+          <span>{props.tasks[2].title}</span>
         </li>
       </ul>
       <div>
