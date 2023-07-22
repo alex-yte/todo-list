28af307318a0007aa9ee15e75e27f9e7a0a8a723
Denis Chirkov <den.alex.chirkov@gmail.com>
2023-07-22 10:01:27 +0200

    move todo to a separate component

---
+++ src/Todolist.tsx
@@ -0,0 +1,32 @@
+function Todolist() {
+  return (
+    <div>
+      <h3>What to learn</h3>
+      <div>
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type="checkbox" checked={true} />
+          <span>HTML</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={true} />
+          <span>CSS</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={false} />
+          <span>React</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todolist;
